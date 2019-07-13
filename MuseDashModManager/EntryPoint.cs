c0ad6882eebf0f30cdfc511d0ac1e3ec3f4a62c9
Lyt99 <lyt999131@163.com>
2019-07-13 17:25:21 +0800

    first commit

---
+++ MuseDashModManager/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MuseDashModManager
+{
+    public class EntryPoint
+    {
+
+        private static bool hasInited = false;
+
+        public static void InitManager()
+        {
+            if (hasInited) return;
+            hasInited = true;
+            Patches.CustomMap.Init();
+            // 安装 Patch
+            Patch.InstallPatches();
+        }
+
+        public static object LoadCustomAssetProxy(string name)
+        {
+            switch (name)
+            {
+                case "AlbumCustom":
+                    return Patches.CustomMap.CreateCustomMusicCells();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ MuseDashModManager/Patch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Harmony;
+
+namespace MuseDashModManager
+{
+    class Patch
+    {
+        private static HarmonyInstance harmonyInstance = HarmonyInstance.Create("pw.baka.musedash.modmanager");
+
+        public static void InstallPatches()
+        {
+            // CustomMap
+            MethodInfo methodInitAlbumDatas = AccessTools.Method(typeof(Assets.Scripts.UI.Panels.PnlStage), "InitAlbumDatas");
+            MethodInfo methodPathIADPostfix = AccessTools.Method(typeof(MuseDashModManager.Patches.CustomMap), "InitAlbumDatasPostfix");
+            harmonyInstance.Patch(methodInitAlbumDatas, null, new HarmonyMethod(methodPathIADPostfix), null);
+
+            MethodInfo methodRebuildChildren = AccessTools.Method(typeof(Assets.Scripts.PeroTools.Nice.Components.FancyScrollView), "RebuildChildren");
+            MethodInfo methodRCPrefix = AccessTools.Method(typeof(MuseDashModManager.Patches.CustomMap), "FancyScrollAlbumRebuildChildrenPrefix");
+            harmonyInstance.Patch(methodRebuildChildren, new HarmonyMethod(methodRCPrefix), null, null);
+        }
+    }
+}
